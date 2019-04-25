2f3ef186563741b00d1294a232fe603788c748cc
P M <przemyslaw.musialik@soprasteria.com>
2019-04-25 09:39:06 +0200

    Add basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact></Redirect>
+        <Route path="/auth" component={AuthPage}></Route>
+        <Route path="/events" component={EventsPage}></Route>
+        <Route path="/bookings" component={BookingsPage}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
